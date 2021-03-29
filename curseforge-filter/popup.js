4e930cedb6701a152da6c13bfebdec63e793a34a
Jace O'Brien <servicefabricdev@gmail.com>
2021-03-29 16:36:18 -0500

    Add thinking spinner

--- curseforge-filter/popup.js
+++ curseforge-filter/popup.js
@@ -6,6 +6,10 @@ filterInput.addEventListener("keyup", async event => {
     if (event.keyCode === enterKey) {
         // Cancel the default action, if needed
         event.preventDefault();
+
+        // Activate waiting indicator
+        toggleIndicators(true, false, false);
+
         chrome.tabs.query(
             {
                 currentWindow: true,
@@ -18,17 +22,35 @@ filterInput.addEventListener("keyup", async event => {
                 var exists = await searchService.nameExists(modpack, input)
                 if (exists) {
                     console.log("Found pack: " + input);
-                    var existsIndicator = document.getElementById("ExistsIndicator");
-                    var notExistsIndicator = document.getElementById("NotExistsIndicator");
-                    existsIndicator.removeAttribute("hidden");
-                    notExistsIndicator.setAttribute("hidden", true);
+                    toggleIndicators(false, true, false);
                 } else {
                     console.log("Could not find pack: " + input);
-                    var existsIndicator = document.getElementById("ExistsIndicator");
-                    var notExistsIndicator = document.getElementById("NotExistsIndicator");
-                    existsIndicator.setAttribute("hidden", true);
-                    notExistsIndicator.removeAttribute("hidden");
+                    toggleIndicators(false, false, true)
                 }
             });
     }
 });
+
+function toggleIndicators(thinking, exists, notExists) {
+    var thinkingIndicator = document.getElementById("ThinkingIndicator");
+    var existsIndicator = document.getElementById("ExistsIndicator");
+    var notExistsIndicator = document.getElementById("NotExistsIndicator");
+
+    if (thinking) {
+        thinkingIndicator.removeAttribute("hidden");
+    } else {
+        thinkingIndicator.setAttribute("hidden", true);
+    }
+
+    if (exists) {
+        existsIndicator.removeAttribute("hidden");
+    } else {
+        existsIndicator.setAttribute("hidden", true);
+    }
+
+    if (notExists) {
+        notExistsIndicator.removeAttribute("hidden");
+    } else {
+        notExistsIndicator.setAttribute("hidden", true);
+    }
+}
